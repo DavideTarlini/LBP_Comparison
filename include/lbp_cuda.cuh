38959e08e54853a78ba0fb15849b1fe0767e0a9e
Davide <davide.tarlini@gmail.com>
2025-02-09 19:25:39 +0100

    Commit

--- include/lbp_cuda.cuh
+++ include/lbp_cuda.cuh
@@ -4,11 +4,7 @@
 #include <cuda_runtime.h>
 #include "../include/util.h"
 
-__global__ void _lbp_kernel_t(int* image, int* result, int rows, int cols);
-__global__ void _lbp_kernel(int* image, int* result, int rows, int cols);
 __global__ void _lbp_h_kernel(int* image, unsigned int* histogram, int rows, int cols);
-__global__ void _histogram_kernel(int* lbp_image, int* histogram, int rows, int cols);
-unsigned int* _histogram_cuda(int* lbp_image, int cols, int rows);
 results get_LBP_hist_cuda(int* image, int rows, int cols);
 
 #endif
--- src/lbp_cuda.cu
+++ src/lbp_cuda.cu
@@ -1,77 +1,6 @@
 #include "../include/lbp_cuda.cuh"
 #include <iostream>
 
-/*#define TILE_SIZE (34*34)
-__global__ void _lbp_kernel_t(int* image, int* result, int rows, int cols){
-    __shared__ unsigned int tile[TILE_SIZE];
-    const int r = blockIdx.y*blockDim.y + threadIdx.y;
-    const int c = blockIdx.x*blockDim.x + threadIdx.x;
-
-    if(r < rows && c < cols){
-        const int padded_local_pos = (threadIdx.y+1)*(blockDim.x+2) + (threadIdx.x+1);
-        const int global_pos = r*cols + c;
-        const int global_padded_pos = (r+1)*(cols+2) + (c+1);
-        unsigned int lbp_value = 0;
-
-        tile[padded_local_pos] = image[global_padded_pos];
-
-        if(threadIdx.y == 0){
-            tile[(threadIdx.y)*(blockDim.x+2) + (threadIdx.x+1)] = image[(r)*(cols+2) + (c+1)];
-        }else if (threadIdx.y == blockDim.y)
-        {
-            tile[(threadIdx.y+2)*(blockDim.x+2) + (threadIdx.x+1)] = image[(r+2)*(cols+2) + (c+1)];
-        }
-
-        if(threadIdx.x == 0){
-            tile[(threadIdx.y+1)*(blockDim.x+2) + (threadIdx.x)] = image[(r+1)*(cols+2) + (c)];
-        }else if (threadIdx.x == blockDim.x)
-        {
-            tile[(threadIdx.y+1)*(blockDim.x+2) + (threadIdx.x+2)] = image[(r+1)*(cols+2) + (c+2)];
-        }
-
-        unsigned int center = tile[padded_local_pos];
-        
-        __syncthreads();
-
-        lbp_value |= (tile[(threadIdx.y)*(blockDim.x+2) + (threadIdx.x)] >= center) << 7;
-        lbp_value |= (tile[(threadIdx.y)*(blockDim.x+2) + (threadIdx.x+1)] >= center) << 6;
-        lbp_value |= (tile[(threadIdx.y)*(blockDim.x+2) + (threadIdx.x+2)] >= center) << 5;
-        lbp_value |= (tile[(threadIdx.y+1)*(blockDim.x+2) + (threadIdx.x+2)] >= center) << 4;
-        lbp_value |= (tile[(threadIdx.y+2)*(blockDim.x+2) + (threadIdx.x+2)] >= center) << 3;
-        lbp_value |= (tile[((threadIdx.y+2)*(blockDim.x+2) + (threadIdx.x+1))] >= center) << 2;
-        lbp_value |= (tile[(threadIdx.y+2)*(blockDim.x+2) + (threadIdx.x)] >= center) << 1;
-        lbp_value |= (tile[(threadIdx.y+1)*(blockDim.x+2) + (threadIdx.x)] >= center) << 0;
-        
-
-        result[global_pos] = lbp_value;
-    }
-}*/
-
-__global__ void _lbp_kernel(int* image, int* result, int rows, int cols){
-    const int r = blockIdx.y*blockDim.y + threadIdx.y;
-    const int c = blockIdx.x*blockDim.x + threadIdx.x;
-
-    if(r < rows && c < cols){
-        const int pos = r*cols + c;
-        const int padded_pos = (r+1)*(cols+2) + (c+1);
-        unsigned int lbp_value = 0;
-
-        const unsigned int center = image[padded_pos];
-
-        lbp_value |= (image[r*(cols+2) + c] >= center) << 7;
-        lbp_value |= (image[r*(cols+2) + (c+1)] >= center) << 6;
-        lbp_value |= (image[r*(cols+2) + (c+2)] >= center) << 5;
-        lbp_value |= (image[(r+1)*(cols+2) + (c+2)] >= center) << 4;
-        lbp_value |= (image[(r+2)*(cols+2) + (c+2)] >= center) << 3;
-        lbp_value |= (image[(r+2)*(cols+2) + (c+1)] >= center) << 2;
-        lbp_value |= (image[(r+2)*(cols+2) + c] >= center) << 1;
-        lbp_value |= (image[(r+1)*(cols+2) + c] >= center) << 0;
-        
-
-        result[pos] = lbp_value;
-    }
-}
-
 __global__ void _lbp_h_kernel(int* image, unsigned int* histogram, int rows, int cols){
     __shared__ unsigned int shared_hist[256];
 
@@ -108,44 +37,6 @@ __global__ void _lbp_h_kernel(int* image, unsigned int* histogram, int rows, int
     if(pos <256){
         atomicAdd(&histogram[pos], shared_hist[pos]);
     }
-}
-
-__global__ void _histogram_kernel(int* lbp_image, unsigned int* histogram, int rows, int cols){
-    __shared__ unsigned int shared_hist[256];
-
-    const int r = blockIdx.y*blockDim.y + threadIdx.y;
-    const int c = blockIdx.x*blockDim.x + threadIdx.x;
-    const int pos = r*cols + c;
-
-    if(pos < 256){
-        atomicExch(&shared_hist[pos], 0);
-    }
-
-    if(r < rows && c < cols){
-        atomicAdd(&shared_hist[lbp_image[pos]], 1);
-    }
-
-    __syncthreads();
-
-    //__shared__ unsigned int counter;
-    //counter = 0;
-    
-    //int count = atomicAdd(&counter, 1);
-    if(pos <256){
-        atomicAdd(&histogram[pos], shared_hist[pos]);
-    }
-}
-
-unsigned int* _histogram_cuda(int* lbp_image, int rows, int cols){
-    unsigned int* histogram = new unsigned int[256];
-
-    for(int i=0; i<rows; i++){
-        for(int j=0; j<cols; j++){
-            histogram[lbp_image[(i*cols) + j]] += 1;
-        }
-    }
-
-    return histogram; 
 }
 
 results get_LBP_hist_cuda(int* image, int rows, int cols){
